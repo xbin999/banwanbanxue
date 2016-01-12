463e6819d932b6da85099bb075864ef83ebb308c
xbin999 <xbin999@gmail.com>
2016-01-12 17:06:47 +0800

    add page for feeds

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -8,6 +8,6 @@ class FeedsController < ApplicationController
 private
 
   def find_all_records
-    @all_records = Record.all.order(created: :desc)
+    @all_records = Record.all.paginate(page: params[:page], per_page: 15).order(created: :desc)
   end
 end
