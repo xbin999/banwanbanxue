7dc8baadc16aea9b88dd5aa892233692129518aa
xbin999 <xbin999@gmail.com>
2016-07-31 21:37:53 +0800

    support cookie, word #sentence#

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -10,23 +10,42 @@ class WordsController < ApplicationController
 
   def new
     @event = Event.new
+    @event.name = cookies[:card_name]
+    @event.age = cookies[:card_age]
+    @event.book = cookies[:card_book]
   end
 
   def translate
     @event = Event.new(event_params)
     if @event.valid?
+      cookies[:card_name] = {value: @event.name, expires: 1.year.from_now}
+      cookies[:card_age]  = {value: @event.age, expires: 1.year.from_now}
+      cookies[:card_book] = {value: @event.book, expires: 1.year.from_now}
       @event.save 
 
-      #@source = params[:source][:name]
       @source = @event.words
       logger.debug "translate #{@source}..."
       @target = Hash.new
-      @source.split(" ").each do |word|
-        begin
-          @target[word] = convert(word)
-        rescue URI::InvalidURIError
-          logger.error "Error in translating #{word}, ingnore."
-          next
+
+      if @source.include? "#"
+        # not #To be or not to be# follow #Harry potter, follow me# 
+        @source.scan(/([[:word:]]+)\ *#(.*?)#/).each do |word, sentence|
+          begin
+            @target[word] = convert(word, sentence)
+          rescue URI::InvalidURIError
+            logger.error "Error in translating #{word}, ingnore."
+            next
+          end
+        end
+      else
+        # hello world
+        @source.split(" ").each do |word|
+          begin
+            @target[word] = convert(word)
+          rescue URI::InvalidURIError
+            logger.error "Error in translating #{word}, ingnore."
+            next
+          end
         end
       end
       #render :json => @target.to_json
@@ -34,7 +53,7 @@ class WordsController < ApplicationController
       respond_to do |format|
         format.html
         format.pdf do
-          render pdf: "words",
+          render pdf: "#{@event.name}_#{@event.book}_#{@event.id}",
                  :template => 'words/translate.html.haml',
                  :layout => "pdf.html.haml"
         end
@@ -69,7 +88,7 @@ private
     return target
   end
 
-  def convert(source)
+  def convert(source, sentence=nil)
     target ||= []
     # http://fanyi.youdao.com/openapi.do?keyfrom=XXXXXX&key=XXXXXXXX&type=data&doctype=json&version=1.1&q=hello
     logger.debug "http://fanyi.youdao.com/openapi.do?keyfrom=#{Rails.configuration.mysecrets['keyfrom']}&key=#{Rails.configuration.mysecrets['key']}&type=data&doctype=json&version=1.1&q=#{source}"
@@ -84,6 +103,9 @@ private
       doc['basic']['explains'].each do |link|
         logger.debug "---> translate to #{link}"
         target << link
+      end
+      if sentence != nil
+        target << sentence
       end
     end
 
